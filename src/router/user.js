fdb19a4b2802aae86b3fc73c06ccc922c0eff3bd
Xiang Li <lixiang020317@gmail.com>
2023-09-27 23:36:30 +0100

    first commit

--- src/router/user.js
+++ src/router/user.js
@@ -4,7 +4,7 @@ const router = new Router();
 router.post("/login", async (ctx, next) => {
   const value = ctx.request.body;
   ctx.body = "cookie is set";
-  console.log(value.username);
+  console.log(value.username, value.password);
 });
 
 module.exports = router;
